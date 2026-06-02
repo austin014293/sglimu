e3f8f8c4cf98d677a00f7409580dd50db60947c5
austin014293 <lee.seung.goo42@gmail.com>
2026-06-02 23:26:29 +0900

    11

--- main.js
+++ main.js
@@ -908,24 +908,24 @@ function init3DAnimations() {
     timeline3D.to(carGroup.position, { z: -100, duration: 4 * interval }, 0);
     timeline3D.to(camera.position, { x: 4, y: 1.2, z: -94, duration: 4 * interval }, 0);
 
-    // Slide 4 -> Slide 5 (Simulation slide & transition) (4 * interval to 5 * interval):
+    // Slide 4 -> Slide 5 (Simulation slide & transition) (4 * interval to 4.5 * interval):
     // Car drives into the tunnel (Z = -100 to Z = -900)
     // Camera follows the car (Z = -94 to Z = -894)
     timeline3D.to(carGroup.position, {
         z: -900,
-        duration: 1 * interval,
+        duration: 0.5 * interval,
         ease: "power1.inOut"
     }, 4 * interval);
     
     timeline3D.to(camera.position, {
         z: -894,
-        duration: 1 * interval,
+        duration: 0.5 * interval,
         ease: "power1.inOut"
     }, 4 * interval);
 
-    // Slide 5 -> Slide 17 (5 * interval to 1.0): Car and camera stay inside the tunnel
-    timeline3D.to(carGroup.position, { z: -900, duration: 12 * interval }, 5 * interval);
-    timeline3D.to(camera.position, { z: -894, duration: 12 * interval }, 5 * interval);
+    // Slide 5 -> Slide 17 (4.5 * interval to 1.0): Car and camera stay inside the tunnel
+    timeline3D.to(carGroup.position, { z: -900, duration: 12.5 * interval }, 4.5 * interval);
+    timeline3D.to(camera.position, { z: -894, duration: 12.5 * interval }, 4.5 * interval);
 
     ScrollTrigger.refresh();
 }
